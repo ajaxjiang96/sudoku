eef051625a3ad4a330e712a9735cd5e9478c0cf3
Ajax Jiang <Ajax_Jiang@epam.com>
2019-10-11 12:05:49 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,25 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Sudoku from './sudoku';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {game: new Sudoku()};
+  }
+  render() {
+    this.state ? console.table(this.state.game.table) : console.log("not ready");
+    return (
+      <div className="App">
+        <h1>Sudoku</h1>
+        <Sudoku />
+        <footer>&copy; 2015 Jiacheng Jiang</footer>
+      </div>
+    );
+  }
 }
 
 export default App;
