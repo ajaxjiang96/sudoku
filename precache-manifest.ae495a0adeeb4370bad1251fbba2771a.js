52ae980b8cab9d73250c15848cae3f13d2bd7068
Ajax Jiang <Ajax_Jiang@epam.com>
2019-10-11 13:01:48 +0800

    Updates

---
+++ precache-manifest.ae495a0adeeb4370bad1251fbba2771a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "991661e6a2cb6d4b63bfab7e0cd5e396",
+    "url": "/sudoku/index.html"
+  },
+  {
+    "revision": "976475c61bad421df691",
+    "url": "/sudoku/static/css/main.68af6ff4.chunk.css"
+  },
+  {
+    "revision": "be0b46d6e35077da1424",
+    "url": "/sudoku/static/js/2.d2d2eb01.chunk.js"
+  },
+  {
+    "revision": "976475c61bad421df691",
+    "url": "/sudoku/static/js/main.8069de28.chunk.js"
+  },
+  {
+    "revision": "47b95e5d5d32d522591c",
+    "url": "/sudoku/static/js/runtime-main.d5c7a4bb.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/sudoku/static/media/logo.25bf045c.svg"
+  }
+]);
